663eff260982c8bee2f3d6d9059b3d520b7c0279
Eggert Geir Axelsson <eggertgeir@gmail.com>
2017-10-13 01:00:08 +0000

    Add files via upload

---
+++ VEF3A/verkefni3/resources/views/verk3/books/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>title</title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="/verk3/books/1">bók 1</a></li>
+      <li><a href="/verk3/books/2">bók 2</a></li>
+      <li><a href="/verk3/books/3">bók 3</a></li>
+    </ul>
+  </body>
+</html>
---
+++ VEF3A/verkefni3/resources/views/verk3/books/show.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>title</title>
+  </head>
+  <body>
+    <ul>
+     <?php $id = $id-1; ?>
+      <li><?= $books[$id][0]; ?></li>
+      <li><?= $books[$id][1]; ?></li>
+      <li><?= $books[$id][2]; ?></li>
+      <li><?= $books[$id][3]; ?></li>
+    </ul>
+  </body>
+</html>
